0f101777438dcb99c25caedaa28d5daf968d434e
Alec-hub77 <oleg.ostapiuk@yahoo.com>
2021-12-10 11:13:18 +0200

    stripe key

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const stripe = require('stripe')(process.env.STRIPE_KEY)
+const stripe = require('stripe')("sk_test_51K45XLG6iY0QLlkSUJ0xRI0B08LSAnYq8eqe5i1PCIVcXF4ixT4lgorzKkONeRWla9XFiPPR7Se7Sfluxnwxiy6600XgAb3rZF")
 
 router.post('/payment', (req, res) => {
     stripe.charges.create({
